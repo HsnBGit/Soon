06701bb4b2d5e634398a97ffb6d4fca8979c671f
hassan45-5 <148480926+Hassan45B@users.noreply.github.com>
2024-10-12 14:49:05 +0300

    update again

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Soon./", // YOUR REPO NAME HERE
+  base: "/Soon/", // YOUR REPO NAME HERE
 });
